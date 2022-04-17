c89d7e3a26f77aeb5f976bd6246c085a7b4a86d7
Bilenko <olimp_72@ukr.net>
2022-04-17 16:37:09 +0300

    Lesson6

---
+++ les_6_task_1.py
@@ -0,0 +1,35 @@
+# Определить, какое число в массиве встречается чаще всего.
+#
+# Variant 1
+from sys import getsizeof
+from random import randint
+
+
+def main(n):
+    min_num = 0
+    max_num = 100
+    num = 0
+    max_value = 0
+    array = [randint(min_num, max_num) for _ in range(n)]
+    print(array)
+    d_array = {}
+    for i in array:
+        d_array[i] = d_array.get(i, 0) + 1
+        if d_array[i] > max_value:
+            num = i
+            max_value = d_array[i]
+    if max_value > 1:
+        print(f'Число {num} встречается {max_value} раз(а)')
+    else:
+        print('Нет повторяющихся чисел')
+    return locals()
+
+
+var = main(10)
+sum_size = 0
+for item, value in var.items():
+    if hasattr(value, '__iter__'):
+        for i in value:
+            sum_size += getsizeof(i)
+    sum_size += getsizeof(value)
+print(sum_size)
